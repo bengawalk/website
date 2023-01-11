ff1aed2be88f1989442d3c5c41e8d886da8d968e
Deep <deep@euclidxr.com>
2023-01-11 09:04:46 +0530

    Remove kia. Change Youtube handle

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        kia: resolve(__dirname, 'kia/index.html')
       }
     }
   }
